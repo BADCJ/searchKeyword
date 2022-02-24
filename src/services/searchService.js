7ea8e0a9c88c280a76e00f3c652ed763f2787e68
BADCJ <84081892+BADCJ@users.noreply.github.com>
2022-02-24 22:24:34 +0530

    logger added in search service

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -2,14 +2,18 @@
 const axios = require('axios');
 const { newsApi } = require('../config');
 const searchHistoryDao = require('../dao/searchHistory.dao');
+const logService = require('./loggerService')
 
 class SearchService{
 
     async callNewsApi (keyword) {
 
+        logService.localModelogInfo(`Searching news api for keyword ${keyword}`);
+
         try{
             const result = await axios.get(`https://newsapi.org/v2/everything?q=${keyword}&from=2022-01-24&sortBy=publishedAt&apiKey=${newsApi.apiKey}`);
             let { data } = result;
+            logService.localModelogInfo(`result of search from api =>`,data);
             return { ...data , keyword }
         }
         catch(e){
@@ -22,18 +26,21 @@ class SearchService{
 
         try{
 
-            console.log();
+            logService.localModelogInfo(`reached searchNews in search service with keyword => `,keyword);
 
             // ? search in history first
             let history = await searchHistoryDao.findOne({keyword});
 
             // ? if found return from history
             if( history ){
+                logService.localModelogInfo(`history found in database , sending the data =>`,history);
                 return {
                     dataSource:"DATABASE",
                     history
                 };
             }
+
+            logService.localModelogInfo(`history not found in database , preparing to get the data from news api`);
 
             // ? If not found , go to api
             let result = await this.searchAndcreateSearchHistory(keyword);
